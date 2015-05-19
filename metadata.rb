af813244b4f8cdde5b35fc9ec1d3bc7bf569feec
Eric Shelley <eric@webdesignbakery.com>
2015-05-19 10:22:00 -0600

    Removing git dependency to see if we fallback on chef's git resource

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,8 @@
-name			 "nvm"
+name             "nvm"
 maintainer       "HipSnip"
 maintainer_email "remy@hipsnip.com"
 license          "Apache 2.0"
 description      "Installs nvm, the node version manager"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-supports 		 'ubuntu', ">= 12.04"
-version          "0.1.0"
-
-depends "git", ">= 2.1.0"
+supports         "ubuntu", ">= 12.04"
+version          "0.1.1"
